5bec84417bdcb8a4cf4f6c8ab4416971b0469ffd
Irvan Malik <irvanmalik48@gmail.com>
2022-05-09 03:34:02 +0700

    init: nextjs
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
